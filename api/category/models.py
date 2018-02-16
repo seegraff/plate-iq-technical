36758d21027ffff35b085c205324f62f8309d27a
Chance Graff <cgraffrun@gmail.com>
2018-02-16 02:09:51 -0600

    Implement model for category items

--- api/category/models.py
+++ api/category/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+import uuid
+
+
+class Category(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    name = models.CharField(
+        max_length=128,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+
+    description = models.TextField(
+        blank=False,
+        null=False,
+    )
+
+    created = models.DateField(
+        auto_now_add=True,
+    )
