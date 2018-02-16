b291bb0290b3db9a8c19b3465bfed3032c81e2a0
Chance Graff <cgraffrun@gmail.com>
2018-02-16 09:52:45 -0600

    Implement model for book items

--- api/book/models.py
+++ api/book/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from category.models import Category
+
+import uuid
+
+
+class BookItem(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+
+    author = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+    )
+
+    created = models.DateField(
+        blank=False,
+        null=False,
+    )
+
+    user = models.ForeignKey(
+        User,
+        unique=False,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
+
+    category = models.ForeignKey(
+        Category,
+        unique=False,
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE,
+    )
